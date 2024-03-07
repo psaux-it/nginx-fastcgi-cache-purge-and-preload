dd6029ed813dbfe0eeb64e9863af9c5388c98de4
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-03-07 03:40:27 +0300

    add cache_purger.php

--- nginx-fastcgi-cache-purge-preload.php
+++ nginx-fastcgi-cache-purge-preload.php
@@ -9,8 +9,9 @@ Author URI: https://www.psauxit.com
 License: GPL2
 */
 
-// Include the cache preloader file
+// Include the purge & preload
 require_once plugin_dir_path( __FILE__ ) . 'includes/cache_preloader.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/cache_purger.php';
 
 // Add buttons to WordPress admin bar
 function add_fastcgi_cache_buttons_admin_bar($wp_admin_bar) {
