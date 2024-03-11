346a4931421212c3780f2d100125c98b3b1c80c6
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-03-12 01:40:35 +0300

    update comments

--- js/nginx-fastcgi-cache-purge-preload.js
+++ js/nginx-fastcgi-cache-purge-preload.js
@@ -1,6 +1,16 @@
+/**
+ * JavaScript for Nginx FastCGI Cache and Preload For Wordpress plugin
+ * Description: This JavaScript file contains functions to manage Nginx FastCGI Cache and Preload For Wordpress plugin and interact with WordPress admin dashboard.
+ * Version: 1.0
+ * Author: Hasan CALISIR | hasan.calisir@psauxit.com
+ * Author URI: https://www.psauxit.com
+ * License: GPL2, WordPress copyright GPL.
+ */
+
 jQuery.noConflict();
 
 (function($) {
+    // JavaScript to handle send mail notifications tickbox
     $(document).ready(function() {
         // Update option value when checkbox state changes
         $('#nginx_cache_send_mail').change(function() {
@@ -8,7 +18,7 @@ jQuery.noConflict();
             $.post(nginx_cache_ajax_object.ajaxurl, {
                 action: 'update_send_mail_option',
                 send_mail: isChecked,
-                _wpnonce: nginx_cache_ajax_object.send_mail_nonce // Use localized nonce for send mail option
+                _wpnonce: nginx_cache_ajax_object.send_mail_nonce
             }, function(response) {
                 // Handle response
                 if (response.success) {
@@ -26,13 +36,14 @@ jQuery.noConflict();
             var xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function() {
                 if (xhr.readyState === 4 && xhr.status === 200) {
-                    $('#nginx-cache-logs-container').html(xhr.responseText); // Update this ID based on your actual logs container
+                    $('#nginx-cache-logs-container').html(xhr.responseText);
                 }
             };
             xhr.open('GET', nginx_cache_ajax_object.ajaxurl + '?action=clear_nginx_cache_logs&_wpnonce=' + nginx_cache_ajax_object.nonce, true);
             xhr.send();
         });
 
+        // JavaScript to handle tab switching
         // Run only in the my plugin settings page
         if ($('#nginx_cache_settings_nonce').length > 0) {
             // Initially hide the help content
