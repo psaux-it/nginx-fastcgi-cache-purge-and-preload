2c0a96573ca1544f1bda033f286fbbaa2a510dd7
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-03-14 20:26:46 +0300

    ad drr

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php // Silence is golden
+
+// Default reject regex
+$reject_regex = "/wp-admin/|/wp-includes/|/wp-json/(.*)|/xmlrpc.php|/wp-.*.php|/cart/|/checkout/|/wc-ajax/(.*)|/my-account/(.*)|/wc-api/(.*)|\?|\=|\&|\#";
