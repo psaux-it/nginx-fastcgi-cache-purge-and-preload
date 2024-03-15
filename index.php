1d489fca173de460a18697d68f3f5578dff7b1df
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-03-15 19:11:19 +0300

    latest-pushed wp repo

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php // Silence is golden
+<?php // Silence is golden-psauxit
 
 // Default reject regex
 $reject_regex = "/wp-admin/|/wp-includes/|/wp-json/(.*)|/xmlrpc.php|/wp-.*.php|/cart/|/checkout/|/wc-ajax/(.*)|/my-account/(.*)|/wc-api/(.*)|\?|\=|\&|\#";
