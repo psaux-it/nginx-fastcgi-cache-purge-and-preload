5f8adecf1a1d2802d05d6aec8f142544d8ae261b
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-03-17 03:32:25 +0300

    better handle ACLs checks

--- nginx-fastcgi-cache-purge-preload.php
+++ nginx-fastcgi-cache-purge-preload.php
@@ -73,26 +73,77 @@ function check_wget_availability() {
 }
 add_action('admin_init', 'check_wget_availability');
 
-// Check ACLs for purge operation
-function check_acl_availability() {
+// Check ACL properly configured for purge operations
+// Check the cache path is exists or not
+function pre_checks($directory_exists_flag = false, $empty_directory_flag = false) {
+    $wp_filesystem = initialize_wp_filesystem();
+
+    if ($wp_filesystem === false) {
+        return false; // Return false if WP_Filesystem initialization failed
+    }
+
     $nginx_cache_settings = get_option('nginx_cache_settings');
     $default_cache_path = find_user_home_folder() . '/change-me-nginx';
     $nginx_cache_path = isset($nginx_cache_settings['nginx_cache_path']) ? $nginx_cache_settings['nginx_cache_path'] : $default_cache_path;
 
-    $output = shell_exec("ls -ld \"$nginx_cache_path\" | awk '{print $1}' | grep '+'");
-        if ($output === null) {
-        add_action('admin_notices', 'display_acl_warning');
+    // Check if directory exists
+    if (!$wp_filesystem->is_dir($nginx_cache_path)) {
+        // Directory does not exist
+        if (!$directory_exists_flag) {
+            $directory_exists_flag = true;
+        }
+        if ($directory_exists_flag) {
+            // Display different message for non-existent directory
+            display_pre_check_warning(true, false, false);
+        }
+        return; // No need to continue further checks
+    }
+
+    // Check if main directory has ACL permissions
+    $output = shell_exec("ls -ld \"$nginx_cache_path\" | awk '{print \$1}' | grep '+'");
+    if ($output === null) {
+        // Main directory does not have ACL permissions
+        display_pre_check_warning(false, false, false);
+        return;
+    }
+
+    // Check if directory is empty
+    $files = $wp_filesystem->dirlist($nginx_cache_path);
+    if (empty($files)) {
+        // Directory is empty, but main directory has ACL permissions
+        if (!$empty_directory_flag) {
+            $empty_directory_flag = true;
+        }
+        if ($empty_directory_flag) {
+            display_pre_check_warning(false, true, true);
+            return;
+        }
+    }
+
+    // Check ACL permissions for each file in the directory
+    $output = shell_exec("find \"$nginx_cache_path\" -exec ls -ld {} + | awk '{print \$1}' | grep -v '+'");
+    if (!empty($output)) {
+        display_pre_check_warning(false, false, false);
+        return;
     }
 }
-add_action('admin_init', 'check_acl_availability');
+add_action('admin_init', 'pre_checks');
 
-// Display acl warning message only on the plugin settings page
-function display_acl_warning() {
+// Handle properly global messages
+function display_pre_check_warning($directory_exists_flag = false, $empty_directory_flag = false, $empty_directory_warning = false) {
     $current_page = isset($_GET['page']) ? $_GET['page'] : '';
     if ($current_page === 'nginx_cache_settings') {
         ?>
-        <div class="notice notice-error">
-            <p><?php esc_html_e('ERROR PERMISSON: The purge action may not work as expected due to permission constraints. ACLs have not been applied to the Nginx cache folder to grant permissions to the PHP-FPM user. Kindly refer to the plugins help section for assistance.', 'textdomain'); ?></p>
+        <div class="notice <?php echo $empty_directory_warning ? 'notice-warning' : 'notice-error'; ?>">
+            <p><?php
+                if ($directory_exists_flag) {
+                    esc_html_e('ERROR PATH: The specified Nginx Cache Directory does not exist.');
+                } elseif ($empty_directory_flag) {
+                    esc_html_e('WARNING PERMISSION: Nginx cache directory is empty, but ACL permissions are configured. Please reload the WordPress site to confirm ACL status.');
+                } else {
+                    esc_html_e('ERROR PERMISSION: Purge action will fail due to ACL permission constraints. Apply ACLs to Nginx Cache Folder for PHP-FPM user access. Refer to the plugins help section for assistance!');
+                }
+            ?></p>
         </div>
         <?php
     }
