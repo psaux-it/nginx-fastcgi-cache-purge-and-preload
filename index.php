61bfe3565b2969fdccb253d50d09b9f77d2cd6e7
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-03-20 16:25:05 +0300

    versioning

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php // Silence is golden-psauxit
 
-// Default reject regex
+// Default reject regex - v1.0.2
 $reject_regex = "/wp-admin/|/wp-includes/|/wp-json/(.*)|/xmlrpc.php|/wp-.*.php|/cart/|/checkout/|/wc-ajax/(.*)|/my-account/(.*)|/wc-api/(.*)|\?|\=|\&|\#";
