c41c240d14ccba4d67b3028b80d3d8647dce5151
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-03-28 01:44:18 +0300

    pushed wp repo

--- includes/log.php
+++ includes/log.php
@@ -2,7 +2,7 @@
 /**
  * Logging & status functions for Nginx FastCGI Cache Purge and Preload
  * Description: This file contains Logging & status functions for Nginx FastCGI Cache Purge and Preload
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Hasan ÇALIŞIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
