6c55906a2c8bef7bac0525b5abbce25868807390
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-03-28 01:47:37 +0300

    pushed wp repo

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php // Silence is golden-psauxit
 
-// Default reject regex - v1.0.2
+// Default reject regex - v1.0.3
 $reject_regex = "/wp-admin/|/wp-includes/|/wp-json/(.*)|/xmlrpc.php|/wp-.*.php|/cart/|/checkout/|/wc-ajax/(.*)|/my-account/(.*)|/wc-api/(.*)|\?|\=|\&|\#";
