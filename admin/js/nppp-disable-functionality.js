19cf16f95afe95195a2a60bf9bb42f5ae04d08e3
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-06-29 23:24:43 +0300

    add auto purge to disabled functionality & improve code

--- admin/js/nppp-disable-functionality.js
+++ admin/js/nppp-disable-functionality.js
@@ -8,7 +8,7 @@
  * License: GPL-2.0+
  */
 jQuery(document).ready(function($) {
-    // Select the preload button in the admin bar
+    // Select the buttons in the admin bar
     var preloadButton = $('#wp-admin-bar-preload-cache');
     var purgeButton = $('#wp-admin-bar-purge-cache');
     var statusButton = $('#wp-admin-bar-fastcgi-cache-status');
@@ -35,7 +35,7 @@ jQuery(document).ready(function($) {
         });
     }
 
-     // Check if the purge button exists and disable it
+    // Check if the purge button exists and disable it
     if (statusButton.length > 0) {
         // Disable the button
         statusButton.find('a').css('pointer-events', 'none');
@@ -44,53 +44,61 @@ jQuery(document).ready(function($) {
         });
     }
 
-    // Disable Purge and Preload buttons on settings page
-    $('.nppp-button').addClass('disabled').removeAttr('href');
-
-    // Check if we're on the plugin settings page
+    // Check if we're on the plugin settings page and disable plugin functionality
     if ($('#nppp-nginx-tabs').length > 0) {
+        // Disable Purge and Preload buttons on settings page
+        $('.nppp-button').addClass('disabled').removeAttr('href');
+
         // Disable status and advanced tabs
         $('#nppp-nginx-tabs').tabs({
-            disabled: [1, 2] // Indexes of the tabs to be disabled (starting from 0)
+            disabled: [1, 2]
         });
+
+        // Disable the schedule checkbox
+        $('#nginx_cache_schedule').prop('disabled', true);
+
+        // disable the rest api checkbox
+        $('#nginx_cache_api').prop('disabled', true);
+
+        // disable the schedule set cron button
+        $('#nginx-cache-schedule-set').prop('disabled', true);
+
+        // disable auto preload checkbox
+        $('#nginx_cache_auto_preload').prop('disabled', true);
+
+        // disable send mail checkbox
+        $('#nginx_cache_send_mail').prop('disabled', true);
+
+        // disable auto purge checkbox
+        $('#nginx_cache_purge_on_update').prop('disabled', true);
+
+        // disable reset regex button
+        $('#nginx-regex-reset-defaults').prop('disabled', true);
+
+        // disable clear logs button
+        $('#clear-logs-button').prop('disabled', true);
+
+        // disable generate API key button
+        $('#api-key-button').prop('disabled', true);
+
+        // disable rest API stuff
+        $('#nppp-api-key .nppp-tooltip, #nppp-purge-url .nppp-tooltip, #nppp-preload-url .nppp-tooltip').css({
+            'pointer-events': 'none',
+            'opacity': '0.5',
+            'cursor': 'not-allowed'
+        }).off('click').on('click', function(event) {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+        });
+
+        // style cron status heading
+        $('.nppp-active-cron-heading').css({
+            'pointer-events': 'none',
+            'opacity': '0.5',
+            'cursor': 'not-allowed'
+        });
+
+        // disable main form submit button
+        $('input[type="submit"][name="submit"].button-primary').prop('disabled', true);
     }
-
-    // Disable the schedule checkbox
-    $('#nginx_cache_schedule').prop('disabled', true);
-
-    // disable the rest api checkbox
-    $('#nginx_cache_api').prop('disabled', true);
-
-    // disable the schedule set cron button
-    $('#nginx-cache-schedule-set').prop('disabled', true);
-
-    // disable auto preload checkbox
-    $('#nginx_cache_auto_preload').prop('disabled', true);
-
-    // disable send mail checkbox
-    $('#nginx_cache_send_mail').prop('disabled', true);
-
-    // disable reset regex button
-    $('#nginx-regex-reset-defaults').prop('disabled', true);
-
-    // disable clear logs button
-    $('#clear-logs-button').prop('disabled', true);
-
-    // disable clear logs button
-    $('#api-key-button').prop('disabled', true);
-
-    // disable rest url stuff
-    $('#nppp-api-key .nppp-tooltip, #nppp-purge-url .nppp-tooltip, #nppp-preload-url .nppp-tooltip').css({
-        'pointer-events': 'none',
-        'opacity': '0.5',
-        'cursor': 'not-allowed'
-     }).off('click').on('click', function(event) {
-        event.preventDefault();
-    });
-
-    $('.nppp-active-cron-heading').css({
-        'pointer-events': 'none',
-        'opacity': '0.5',
-        'cursor': 'not-allowed'
-    });
 });
