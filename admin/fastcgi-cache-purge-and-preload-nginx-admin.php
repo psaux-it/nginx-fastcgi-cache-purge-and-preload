16bf53f9c6e1b6c392905ed74bb6ca0730a22adb
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-08-05 20:04:59 +0300

    add nppp_restart_systemd_service ajax action

--- admin/fastcgi-cache-purge-and-preload-nginx-admin.php
+++ admin/fastcgi-cache-purge-and-preload-nginx-admin.php
@@ -76,6 +76,7 @@ add_action('npp_cache_preload_event', 'nppp_create_scheduled_event_preload_callb
 add_action('npp_cache_preload_status_event', 'nppp_create_scheduled_event_preload_status_callback');
 add_action('wp_ajax_nppp_get_active_cron_events_ajax', 'nppp_get_active_cron_events_ajax');
 add_action('wp_ajax_nppp_clear_plugin_cache', 'nppp_clear_plugin_cache_callback');
+add_action('wp_ajax_nppp_restart_systemd_service', 'nppp_restart_systemd_service');
 add_action('save_post', 'nppp_purge_cache_on_update');
 add_action('wp_insert_comment', 'nppp_purge_cache_on_comment', 200, 2);
 add_action('transition_comment_status', 'nppp_purge_cache_on_comment_change', 200, 3);
