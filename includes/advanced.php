faa6bfc6adc65757751f8689e8dafe36cfe7f03f
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-08-09 03:19:58 +0300

    handle all exceptions

--- includes/advanced.php
+++ includes/advanced.php
@@ -331,7 +331,7 @@ function nppp_extract_cached_urls($nginx_cache_path) {
                 }
             }
         }
-    } catch (UnexpectedValueException $e) {
+    } catch (Exception $e) {
         return "<div class=\"nppp-premium-wrap\"><h2>Advanced Tab</h2><p>ERROR PERMISSION: Unable to access directory $nginx_cache_path. Refer to -Help- tab for guidance.</p></div>";
     }
 
