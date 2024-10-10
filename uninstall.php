1e5b5acfdefb9506d9dacde80d2efdbc32d76f9a
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-10-10 09:40:27 +0300

    pushed wp repo

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 /**
  * Uninstallation script for FastCGI Cache Purge and Preload for Nginx
  * Description: This file handles the cleanup process when the FastCGI Cache Purge and Preload for Nginx plugin is uninstalled.
- * Version: 2.0.3
+ * Version: 2.0.4
  * Author: Hasan ÇALIŞIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
