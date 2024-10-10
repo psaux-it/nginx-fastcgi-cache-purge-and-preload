aa6abf9594c7184251d0de3c9d6d68561c503eea
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-10-10 09:22:08 +0300

    pushed wp repo

--- includes/configuration-parser.php
+++ includes/configuration-parser.php
@@ -2,7 +2,7 @@
 /**
  * Nginx config parser functions for FastCGI Cache Purge and Preload for Nginx
  * Description: This file contains Nginx config parser functions for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.0.3
+ * Version: 2.0.4
  * Author: Hasan ÇALIŞIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
