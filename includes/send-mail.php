b5b06f6dfb93fa2d2c469f02ad3e4a9f3e4e83f0
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-10-10 09:37:56 +0300

    pushed wp repo

--- includes/send-mail.php
+++ includes/send-mail.php
@@ -2,7 +2,7 @@
 /**
  * Send mail code for FastCGI Cache Purge and Preload for Nginx
  * Description: This file contains send mail code for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.0.3
+ * Version: 2.0.4
  * Author: Hasan ÇALIŞIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
