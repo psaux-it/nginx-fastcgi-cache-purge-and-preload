f8441f7d8e7cf098137645de810e2c2cb85b8a5a
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-10-10 09:36:18 +0300

    pushed wp repo

--- includes/rest-api-helper.php
+++ includes/rest-api-helper.php
@@ -2,7 +2,7 @@
 /**
  * REST API Helper for FastCGI Cache Purge and Preload for Nginx
  * Description: This file contains REST API related code for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.0.3
+ * Version: 2.0.4
  * Author: Hasan ÇALIŞIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
