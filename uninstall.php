9f948db7db5b9fdb08ce62d09e8f8eb7efce7ca5
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-11-13 12:44:54 +0300

    clear npp cache on uninstall

--- uninstall.php
+++ uninstall.php
@@ -14,5 +14,50 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
 }
 
+// Clear all transients related to the NPP
+function nppp_clear_plugin_cache_on_uninstall() {
+    $static_key_base = 'nppp';
+
+    // Transients to clear
+    $transients = array(
+        'nppp_cache_keys_wpfilesystem_error',
+        'nppp_nginx_conf_not_found',
+        'nppp_cache_keys_not_found',
+        'nppp_cache_keys_' . md5($static_key_base),
+        'nppp_bindfs_version_' . md5($static_key_base),
+        'nppp_libfuse_version_' . md5($static_key_base),
+        'nppp_permissions_check_' . md5($static_key_base),
+    );
+
+    // Category-related transients based on the URL cache
+    $url_cache_pattern = 'nppp_category_';
+
+    // Rate limit transients
+    $rate_limit_pattern = 'nppp_rate_limit_';
+
+    // Get all transients
+    $all_transients = wp_cache_get('alloptions', 'options');
+    foreach ($all_transients as $transient_key => $value) {
+        // Match the category-based transients
+        if (strpos($transient_key, $url_cache_pattern) !== false) {
+            $transients[] = $transient_key;
+        }
+
+        // Match the rate limit-related transients
+        if (strpos($transient_key, $rate_limit_pattern) !== false) {
+            $transients[] = $transient_key;
+        }
+    }
+
+    // Attempt to delete all transients
+    foreach ($transients as $transient) {
+        // Delete the transient
+        delete_transient($transient);
+    }
+}
+
+// Delete plugin transients
+nppp_clear_plugin_cache_on_uninstall();
+
 // Delete plugin options
 delete_option('nginx_cache_settings');
