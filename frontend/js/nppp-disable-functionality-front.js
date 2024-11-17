6312b7faa459c8071d68d67bfe50e5de2ae60c5a
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-11-18 02:07:49 +0300

    pushed wp repo

--- frontend/js/nppp-disable-functionality-front.js
+++ frontend/js/nppp-disable-functionality-front.js
@@ -1,7 +1,7 @@
 /**
  * Frontend JavaScript for FastCGI Cache Purge and Preload for Nginx
  * Description: This JavaScript file contains functions that disabling frontpage admin bar actions for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.0.4
+ * Version: 2.0.5
  * Author: Hasan ÇALIŞIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
