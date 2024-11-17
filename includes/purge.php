86d39c042b4725e1487c19a6b4a6d2aa35da41a5
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-11-18 01:41:46 +0300

    change hook name

--- includes/purge.php
+++ includes/purge.php
@@ -499,7 +499,8 @@ function nppp_purge($nginx_cache_path, $PIDFILE, $tmp_path, $nppp_is_rest_api = 
     }
 
     // Fire the 'nppp_purged' action, triggering any other plugin actions that are hooked into this event
-    do_action('nppp_purged');
+    // If auto preload is enabled this hook will create both NPP cache and compatible plugin cache at the same time
+    do_action('nppp_purged_all');
 
     // If set call preload immediately after purge
     if ($auto_preload) {
