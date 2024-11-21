f2be88e48d8fcfa743dcbb9cc50cc6989bc065f1
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-11-21 05:31:55 +0300

    pushed wp repo

--- admin/js/nppp-disable-functionality.js
+++ admin/js/nppp-disable-functionality.js
@@ -1,7 +1,7 @@
 /**
  * JavaScript for FastCGI Cache Purge and Preload for Nginx
  * Description: This file contains disable functionality for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.0.5
+ * Version: 2.0.6
  * Author: Hasan ÇALIŞIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
