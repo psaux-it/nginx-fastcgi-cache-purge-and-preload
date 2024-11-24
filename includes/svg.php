c326162f0080eda31c0c3e3f5800bbdb050090ca
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-11-24 07:06:51 +0300

    pushed wp repo

--- includes/svg.php
+++ includes/svg.php
@@ -2,8 +2,8 @@
 /**
  * SVG icon code for FastCGI Cache Purge and Preload for Nginx
  * Description: This file contains svg icon code for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.0.7
- * Author: Hasan ÇALIŞIR
+ * Version: 2.0.8
+ * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
  * License: GPL-2.0+
