3f7185753faf1a0eeaf3908b18e143b358376edc
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-11-28 15:07:35 +0300

    pushed wp repo

--- includes/advanced.php
+++ includes/advanced.php
@@ -2,7 +2,7 @@
 /**
  * Advanced table for FastCGI Cache Purge and Preload for Nginx
  * Description: This file contains advanced table functions for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.0.8
+ * Version: 2.0.9
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
