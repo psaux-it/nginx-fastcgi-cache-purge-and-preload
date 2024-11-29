4f493e60a3eaf735a6ac80551492cacc27eb530b
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-11-29 16:17:36 +0300

    use transition_post_status instead of save_post hook

--- admin/fastcgi-cache-purge-and-preload-nginx-admin.php
+++ admin/fastcgi-cache-purge-and-preload-nginx-admin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       FastCGI Cache Purge and Preload for Nginx
  * Plugin URI:        https://github.com/psaux-it/nginx-fastcgi-cache-purge-and-preload
  * Description:       Manage FastCGI Cache Purge and Preload for Nginx operations directly from your WordPress admin dashboard.
- * Version:           2.0.9
+ * Version:           2.0.8
  * Author:            Hasan CALISIR
  * Author URI:        https://www.psauxit.com/
  * Author Email:      hasan.calisir@psauxit.com
@@ -108,7 +108,7 @@ add_action('npp_cache_preload_status_event', 'nppp_create_scheduled_event_preloa
 add_action('wp_ajax_nppp_get_active_cron_events_ajax', 'nppp_get_active_cron_events_ajax');
 add_action('wp_ajax_nppp_clear_plugin_cache', 'nppp_clear_plugin_cache_callback');
 add_action('wp_ajax_nppp_restart_systemd_service', 'nppp_restart_systemd_service');
-add_action('save_post', 'nppp_purge_cache_on_update', 10, 3);
+add_action('transition_post_status', 'nppp_purge_cache_on_update', 10, 3);
 add_action('wp_insert_comment', 'nppp_purge_cache_on_comment', 200, 2);
 add_action('transition_comment_status', 'nppp_purge_cache_on_comment_change', 200, 3);
 add_action('admin_post_save_nginx_cache_settings', 'nppp_handle_nginx_cache_settings_submission');
