d4a481eea6bf0b29d03cb30935efe02cb348484b
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-11-29 16:27:24 +0300

    pushed wp repo

--- includes/log.php
+++ includes/log.php
@@ -2,7 +2,7 @@
 /**
  * Logging & WP admin notices function for FastCGI Cache Purge and Preload for Nginx
  * Description: This file contain logging & wp admin notices function for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.0.8
+ * Version: 2.0.9
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
