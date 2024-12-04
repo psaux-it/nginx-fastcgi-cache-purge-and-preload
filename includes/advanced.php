044fd51020ba6c7a0e2571c6baf691a4ebf0fdc7
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-12-04 17:03:55 +0300

    fix indentation

--- includes/advanced.php
+++ includes/advanced.php
@@ -100,7 +100,7 @@ function nppp_premium_html($nginx_cache_path) {
     // Warn about cache keys not found
     if ($config_data === false) {
         echo '<div class="nppp-premium-wrap">
-                   <p class="nppp-advanced-error-message">' . wp_kses_post( __( 'INFO CACHE KEY: No <span style="color: #f0c36d;">fastcgi_cache_key</span> directive was found. This may indicate a <span style="color: #f0c36d;">parsing error</span> or a missing <span style="color: #f0c36d;">nginx.conf</span> file.', 'fastcgi-cache-purge-and-preload-nginx' ) ) . '</p>
+                  <p class="nppp-advanced-error-message">' . wp_kses_post( __( 'INFO CACHE KEY: No <span style="color: #f0c36d;">fastcgi_cache_key</span> directive was found. This may indicate a <span style="color: #f0c36d;">parsing error</span> or a missing <span style="color: #f0c36d;">nginx.conf</span> file.', 'fastcgi-cache-purge-and-preload-nginx' ) ) . '</p>
               </div>';
     // Warn about cache keys not found
     } elseif (isset($config_data['cache_keys']) && $config_data['cache_keys'] === ['Not Found']) {
