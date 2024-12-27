f72152fa1d05ac5b475ba09b33f92e5eb4445545
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2024-12-27 20:38:35 +0300

    update message

--- includes/purge.php
+++ includes/purge.php
@@ -194,7 +194,7 @@ function nppp_purge_single($nginx_cache_path, $current_page_url, $nppp_auto_purg
                                 $error_message = __( 'ERROR PATH: The Nginx cache path appears to be a critical system directory or a first-level directory. Failed to purge Nginx cache!', 'fastcgi-cache-purge-and-preload-nginx' );
                                 break;
                             case 'file_not_found_or_not_readable':
-                                $error_message = __( 'ERROR PATH: The specified Nginx cache path does not exist. Failed to purge Nginx cache', 'fastcgi-cache-purge-and-preload-nginx' );
+                                $error_message = __( 'ERROR PATH: The specified Nginx cache path was not found. Failed to purge Nginx cache', 'fastcgi-cache-purge-and-preload-nginx' );
                                 break;
                             default:
                                 $error_message = __( 'ERROR PATH: An invalid Nginx cache path was provided. Failed to purge Nginx cache', 'fastcgi-cache-purge-and-preload-nginx' );
