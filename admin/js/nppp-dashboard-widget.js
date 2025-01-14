c1a9eae874eaa7f5d67ae587ba997019a57d430f
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-01-14 17:33:15 +0300

    Create nppp-dashboard-widget.js
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

---
+++ admin/js/nppp-dashboard-widget.js
@@ -0,0 +1,42 @@
+/**
+ * JavaScript for FastCGI Cache Purge and Preload for Nginx
+ * Description: This file contains wp dashboard widget preloader effect for FastCGI Cache Purge and Preload for Nginx
+ * Version: 2.0.9
+ * Author: Hasan CALISIR
+ * Author Email: hasan.calisir@psauxit.com
+ * Author URI: https://www.psauxit.com
+ * License: GPL-2.0+
+ */
+
+jQuery(document).ready(function ($) {
+    const $preloader = $('#nppp-loader-overlay');
+
+    // Function to show the preloader overlay
+    // Adds the 'active' class and fades in the preloader over 50 milliseconds
+    function showPreloader() {
+        $('.nppp-loader-fill').css({
+            'animation': 'nppp-fill 2s ease-in-out infinite'
+        });
+
+        // Remove backdrop filters from `#nppp-loader-overlay` by setting them to 'none'
+        $('#nppp-loader-overlay').css({
+            'backdrop-filter': 'none',
+            '-webkit-backdrop-filter': 'none',
+            'transition': 'none'
+        });
+
+        // Add 'active' class and fade in the preloader
+        $preloader.addClass('active').fadeIn(50);
+    }
+
+    // Show preloader on widget actions
+    $('.nppp-action-button').on('click', function (e) {
+        const action = $(this).data('action');
+
+        if (action === 'nppp-widget-purge') {
+            showPreloader();
+        } else if (action === 'nppp-widget-preload') {
+            showPreloader();
+        }
+    });
+});
