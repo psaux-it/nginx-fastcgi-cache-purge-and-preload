bcbc078c69139f4fd6b097a693ce72d29249e9ba
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-01-23 03:03:27 +0300

    display the status of the ongoing Preload action
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- admin/js/nppp-dashboard-widget.js
+++ admin/js/nppp-dashboard-widget.js
@@ -11,7 +11,7 @@
 // NPP WP dashboard widget
 (function ($) {
     'use strict';
-    
+
     $(document).ready(function () {
         const $preloader = $('#nppp-loader-overlay');
 
@@ -46,5 +46,80 @@
         $('.nppp-action-trigger').on('click', function() {
             showPreloader();
         });
+
+        // Display status of ongoing Preload action
+        function npppCheckPreloadStatus() {
+            var npppPreloadInProgress = $('#nppp-preload-in-progress').length > 0;
+
+            // Get the Preload All button related elements
+            var npppPreloadButton = $('a[data-action="nppp-widget-preload"]');
+            var npppPreloadIcon = npppPreloadButton.find('span');
+
+            // Set the default styles
+            var defaultStyles = {
+                'font-size': '14px',
+                'color': 'white',
+                'background-color': '#3CB371',
+                'padding': '8px 12px',
+                'text-decoration': 'none',
+                'font-weight': '500',
+                'display': 'inline-flex',
+                'align-items': 'center',
+                'justify-content': 'center',
+                'transition': 'background-color 0.3s ease',
+                'flex': '48%',
+                'opacity': '1',
+                'pointer-events': 'auto',
+            };
+
+            // Check for the ongoing Preload process
+            if (npppPreloadInProgress) {
+                npppPreloadButton.css('color', 'orange');
+                npppPreloadButton.css('pointer-events', 'none');
+                //npppPreloadButton.css('transition', 'none');
+                npppPreloadButton.prop('href', '#');
+
+                // Start blinking effect
+                npppstartBlinking(npppPreloadButton);
+
+                // Change icon to clock
+                npppPreloadIcon.removeClass('dashicons-update').addClass('dashicons-clock');
+            } else {
+                // Get the current background color
+                var currentBackgroundColor = npppPreloadButton.css('background-color');
+
+                // Only revert back style if changed before
+                if (currentBackgroundColor !== 'rgb(60, 179, 113)') {
+
+                    // Apply the default styles
+                    npppPreloadButton.css(defaultStyles);
+
+                    // Apply default href
+                    npppPreloadButton.prop('href', function() {
+                        return $('a[data-action="nppp-widget-preload"]').data('href');
+                    });
+
+                    // Stop the blinking effect
+                    npppstopBlinking(npppPreloadButton);
+
+                    // Revert icon back
+                    npppPreloadIcon.removeClass('dashicons-clock').addClass('dashicons-update');
+                }
+            }
+        }
+
+        // Function to start blinking using CSS animation
+        function npppstartBlinking(element) {
+            element.css('animation', 'nppp-blink 2s infinite alternate');
+            element.css('animation-timing-function', 'ease-in-out');
+        }
+
+        // Function to stop blinking by removing the animation
+        function npppstopBlinking(element) {
+            element.css('animation', 'none');
+        }
+
+        // Run the check on page load and when the DOM is ready
+        npppCheckPreloadStatus();
     });
 })(jQuery);
