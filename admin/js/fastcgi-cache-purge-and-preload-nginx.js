3758986b57feb3bd369fdfb1701974db224cedae
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-06-23 02:43:00 +0300

    use insertAdjacentHTML instead of append
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- admin/js/fastcgi-cache-purge-and-preload-nginx.js
+++ admin/js/fastcgi-cache-purge-and-preload-nginx.js
@@ -2635,7 +2635,7 @@ function npppupdateStatus() {
     }
 
     npppLibfuseVersionSpan.appendChild(iconSpanLibfuse);
-    npppLibfuseVersionSpan.append(libfuseStatusText);
+    npppLibfuseVersionSpan.insertAdjacentHTML('beforeend', libfuseStatusText);
 
     // Update the FUSE status for bindfs
     var npppBindfsVersionSpan = document.getElementById("npppBindfsVersion");
@@ -2687,7 +2687,7 @@ function npppupdateStatus() {
     }
 
     npppBindfsVersionSpan.appendChild(iconSpanBindfs);
-    npppBindfsVersionSpan.append(bindfsStatusText);
+    npppBindfsVersionSpan.insertAdjacentHTML('beforeend', bindfsStatusText);
 
     // Fetch and update permission isolation status
     var nppppermIsolationSpan = document.getElementById("nppppermIsolation");
