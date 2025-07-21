9d089c10cc6a24d80dd6d8a76c7f5f767139e639
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-07-21 19:47:52 +0300

    add nppp_est_url_counts_ to delete list
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- uninstall.php
+++ uninstall.php
@@ -32,6 +32,7 @@ function nppp_clear_plugin_cache_on_uninstall() {
         'nppp_cache_paths_' . md5($static_key_base),
         'nppp_fuse_paths_' . md5($static_key_base),
         'nppp_webserver_user_' . md5($static_key_base),
+        'nppp_est_url_counts_' . md5($static_key_base),
     );
 
     // Category-related transients based on the URL cache
