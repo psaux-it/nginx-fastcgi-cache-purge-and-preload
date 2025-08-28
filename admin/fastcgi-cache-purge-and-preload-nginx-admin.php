c357815ea0463ed090d892edc22e20c9f7921a37
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-08-28 19:12:19 +0300

    add ajax action related purge
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- admin/fastcgi-cache-purge-and-preload-nginx-admin.php
+++ admin/fastcgi-cache-purge-and-preload-nginx-admin.php
@@ -182,6 +182,7 @@ add_action('wp_ajax_nppp_update_auto_preload_mobile_option', 'nppp_update_auto_p
 add_action('npp_plugin_tracking_event', 'nppp_plugin_tracking', 10, 1);
 add_action('wp_dashboard_setup', 'nppp_add_dashboard_widget');
 add_action('wp_ajax_nppp_update_enable_proxy_option', 'nppp_update_enable_proxy_option');
+add_action('wp_ajax_nppp_update_related_fields', 'nppp_update_related_fields');
 $nppp_auto_purge
     ? array_map(function($purge_action) { add_action($purge_action, 'nppp_purge_callback'); }, $page_cache_purge_actions)
     : array_map(function($purge_action) { remove_action($purge_action, 'nppp_purge_callback'); }, $page_cache_purge_actions);
