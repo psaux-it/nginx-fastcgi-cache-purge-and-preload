b3f55d6d10ab3ca4b7c12e39caf0165347f06e4b
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-10-04 02:01:55 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/compat-gutenberg.php
+++ includes/compat-gutenberg.php
@@ -2,7 +2,7 @@
 /**
  * Purge action functions for FastCGI Cache Purge and Preload for Nginx
  * Description: Gutenberg integration — purge Nginx cache when posts are saved via REST (block editor).
- * Version: 2.1.3
+ * Version: 2.1.4
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
