751eb0dc0bbc119148358ea427a59faf60994407
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-10-04 02:01:27 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/compat-elementor.php
+++ includes/compat-elementor.php
@@ -2,7 +2,7 @@
 /**
  * Purge action functions for FastCGI Cache Purge and Preload for Nginx
  * Description: Elementor integration — purge Nginx cache when Elementor saves content or regenerates CSS.
- * Version: 2.1.3
+ * Version: 2.1.4
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
