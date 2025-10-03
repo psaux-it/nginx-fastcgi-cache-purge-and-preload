bcac84e9e76d970ce513430ce99afa8327397010
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-10-04 02:12:00 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/related.php
+++ includes/related.php
@@ -3,7 +3,7 @@
  * Related purge helpers for FastCGI Cache Purge and Preload for Nginx
  * Description: Helper functions to purge (and optionally preload) related URLs—homepage and category archives—
  *              whenever a single post/page is purged (via auto purge, front-end action, or Advanced tab).
- * Version: 2.1.3
+ * Version: 2.1.4
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
