1ef3847d7fad2ef3e47eb079d59d0e1de95b80fe
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-10-04 02:18:40 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- admin/js/nppp-disable-preload.js
+++ admin/js/nppp-disable-preload.js
@@ -1,7 +1,7 @@
 /**
  * JavaScript for FastCGI Cache Purge and Preload for Nginx
  * Description: This file contains code to disable Preload features in unsupported environments for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.1.3
+ * Version: 2.1.4
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
