d1cfe8758535ac6dd42045e042fd9edea7958514
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2025-10-04 02:02:48 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/dashboard-widget.php
+++ includes/dashboard-widget.php
@@ -2,7 +2,7 @@
 /**
  * Dashboard widget for FastCGI Cache Purge and Preload for Nginx
  * Description: This file contains dashboard widget functions for FastCGI Cache Purge and Preload for Nginx
- * Version: 2.1.3
+ * Version: 2.1.4
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
