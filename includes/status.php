57975cf22511519a5d9ebe67a892791714ca92d3
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-03-19 20:17:34 +0300

    drop transient
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/status.php
+++ includes/status.php
@@ -82,7 +82,6 @@ function nppp_clear_plugin_cache($silent = false) {
         'nppp_preload_phase_' . md5($static_key_base),
         'nppp_preload_cycle_start_' . md5($static_key_base),
         'nppp_safexec_ok',
-        'nppp_http_purge_module_ok',
         'nppp_cache_purge_module_' . md5( $static_key_base ),
     );
 
