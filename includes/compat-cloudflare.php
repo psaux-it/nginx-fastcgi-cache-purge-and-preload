4f6d9fc4e07e81527928c6f50ebedcca1064097c
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-03-23 09:03:26 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/compat-cloudflare.php
+++ includes/compat-cloudflare.php
@@ -2,7 +2,7 @@
 /**
  * Cloudflare APO compatibility for Nginx Cache Purge Preload
  * Description: Mirrors plugin purge actions to Cloudflare APO to keep edge cache synchronized.
- * Version: 2.1.4
+ * Version: 2.1.5
  * Author: Hasan CALISIR
  * License: GPL-2.0+
  */
