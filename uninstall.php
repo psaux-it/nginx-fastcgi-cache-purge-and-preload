0b1be267545a367b9b090e6ad9d5dd8ecf2d148f
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-03-23 08:54:11 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 /**
  * Uninstall cleanup routines for Nginx Cache Purge Preload
  * Description: Removes plugin options, transients, runtime artifacts, and scheduled events during uninstall.
- * Version: 2.1.4
+ * Version: 2.1.5
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
