1f0f792dfbde6bbdcb791e81b8e6202912a82efd
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-03-23 09:12:03 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/purge-lock.php
+++ includes/purge-lock.php
@@ -2,7 +2,7 @@
 /**
  * Purge operation locking for Nginx Cache Purge Preload
  * Description: Atomic WP_Upgrader-based lock helpers for concurrent purge serialization
- * Version: 2.1.4
+ * Version: 2.1.5
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
