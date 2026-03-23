8137c1632f041f0a38dcfa749750bf6aa6e3356e
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-03-23 09:19:27 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/wp-filesystem.php
+++ includes/wp-filesystem.php
@@ -2,7 +2,7 @@
 /**
  * WordPress filesystem helpers for Nginx Cache Purge Preload
  * Description: Wraps WP_Filesystem operations, logging, and permission-safe file access utilities.
- * Version: 2.1.4
+ * Version: 2.1.5
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
