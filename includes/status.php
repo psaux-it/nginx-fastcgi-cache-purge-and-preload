e0dbe3ae4d51ae2e9ac550dd545f55381533c9aa
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-03-25 13:08:12 +0300

    improve compatibility with Engintron
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/status.php
+++ includes/status.php
@@ -589,7 +589,12 @@ function nppp_get_in_cache_page_count() {
 
             // Accept only GET entries (HEAD/POST/etc. are not cache targets here)
             $key_line = $match[1];
-            if (strpos($key_line, 'GET') === false) {
+            if (strpos($key_line, 'POST') !== false ||
+                strpos($key_line, 'HEAD') !== false ||
+                strpos($key_line, 'PUT') !== false ||
+                strpos($key_line, 'DELETE') !== false ||
+                strpos($key_line, 'PATCH') !== false ||
+                strpos($key_line, 'OPTIONS') !== false) {
                 continue;
             }
 
