5b45b19ee18a1df3c8c288022a40822737e95851
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-03-27 16:40:01 +0300

    add new transient to clear list
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/status.php
+++ includes/status.php
@@ -83,6 +83,7 @@ function nppp_clear_plugin_cache($silent = false) {
         'nppp_http_purge_endpoint_broken',
         'nppp_wget_urls_cache_prev_key',
         'nppp_safexec_ok',
+        'nppp_category_map',
     );
 
     // Delete each known transient
