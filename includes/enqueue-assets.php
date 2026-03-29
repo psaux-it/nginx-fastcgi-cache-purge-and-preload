47b4dbf21aac3bc8e84690cf62acbb77c1bee030
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-03-29 18:41:30 +0300

    update conditionally disable Nginx Cache features
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/enqueue-assets.php
+++ includes/enqueue-assets.php
@@ -248,6 +248,7 @@ function nppp_disable_features($unsupported, $preload) {
             'nppp_related_preload_after_manual'  => 'no',
             'nginx_cache_pctnorm_mode'           => 'off',
             'nppp_http_purge_enabled'            => 'no',
+            'nppp_rg_purge_enabled'              => 'no',
         );
     } elseif ( $preload === true ) {
         // If preload feature not useable
