b66091af46454d1ee6195a9a0a82c42539bb6e82
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-04-27 09:01:52 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/autoload.php
+++ includes/autoload.php
@@ -2,7 +2,7 @@
 /**
  * Class autoloader for Nginx Cache Purge Preload
  * Description: Resolves plugin classes via classmap, WordPress-style paths, and PSR-4 fallback with caching.
- * Version: 2.1.5
+ * Version: 2.1.6
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
