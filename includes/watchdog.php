64a6bfdc52358e49d32796f405d07acec8b8aced
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-04-27 09:14:59 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/watchdog.php
+++ includes/watchdog.php
@@ -4,7 +4,7 @@
  * Description: Monitors the preload server process and executes post-preload completion work.
  *              Ensures post-preload tasks run immediately after cache preloading finishes.
  *              Designed for low-traffic sites where WP-Cron delays can prevent timely execution.
- * Version: 2.1.5
+ * Version: 2.1.6
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
