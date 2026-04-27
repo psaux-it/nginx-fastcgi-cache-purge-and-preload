d84464e6910ee2bd92f9644176e7cc922602a4bb
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-04-27 09:11:49 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/settings-callbacks.php
+++ includes/settings-callbacks.php
@@ -2,7 +2,7 @@
 /**
  * Settings field callbacks for Nginx Cache Purge Preload
  * Description: Renders all individual settings field HTML for the WordPress Settings API.
- * Version: 2.1.5
+ * Version: 2.1.6
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
