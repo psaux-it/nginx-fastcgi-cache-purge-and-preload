87cd0bfb6ddacdf407562ab80ebf9aa0e4aa92b1
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-04-27 09:11:02 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/send-mail.php
+++ includes/send-mail.php
@@ -2,7 +2,7 @@
 /**
  * Email notification handlers for Nginx Cache Purge Preload
  * Description: Sends completion and status emails for preload and related background tasks.
- * Version: 2.1.5
+ * Version: 2.1.6
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
