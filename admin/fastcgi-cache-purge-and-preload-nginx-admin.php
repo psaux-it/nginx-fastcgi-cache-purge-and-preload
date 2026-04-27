04ed6202835ebcd4a916e9821fea65c2e3ab7877
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-04-27 03:00:38 +0300

    Add action for bypassing path restriction
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- admin/fastcgi-cache-purge-and-preload-nginx-admin.php
+++ admin/fastcgi-cache-purge-and-preload-nginx-admin.php
@@ -257,6 +257,7 @@ add_action('wp_dashboard_setup', 'nppp_add_dashboard_widget');
 add_action('wp_ajax_nppp_update_enable_proxy_option', 'nppp_update_enable_proxy_option');
 add_action('wp_ajax_nppp_update_related_fields', 'nppp_update_related_fields');
 add_action('wp_ajax_nppp_update_pctnorm_mode', 'nppp_update_pctnorm_mode');
+add_action('wp_ajax_nppp_update_bypass_path_restriction', 'nppp_update_bypass_path_restriction');
 add_action('wp_ajax_nppp_refresh_cache_ratio', 'nppp_refresh_cache_ratio_callback');
 add_action('nppp_plugin_admin_notices', function($type, $message, $log_message, $display_notice) {
     // Check if admin notice should be displayed
