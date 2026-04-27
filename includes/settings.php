d7a8bb78eb43f687b87b5195f091db5f0b91b691
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-04-27 09:13:30 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/settings.php
+++ includes/settings.php
@@ -2,7 +2,7 @@
 /**
  * Settings for Nginx Cache Purge Preload
  * Description: Loader — delegates to sub-modules.
- * Version: 2.1.5
+ * Version: 2.1.6
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
