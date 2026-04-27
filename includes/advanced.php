1f67aebfa4fe3e5970af75c769e368b914cba391
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-04-27 09:01:30 +0300

    pushed wp repo
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/advanced.php
+++ includes/advanced.php
@@ -2,7 +2,7 @@
 /**
  * Advanced tab handlers for Nginx Cache Purge Preload
  * Description: Implements advanced admin actions for targeted URL purge and preload tasks.
- * Version: 2.1.5
+ * Version: 2.1.6
  * Author: Hasan CALISIR
  * Author Email: hasan.calisir@psauxit.com
  * Author URI: https://www.psauxit.com
