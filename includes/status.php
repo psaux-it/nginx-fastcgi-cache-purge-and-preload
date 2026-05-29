ddc5c6244c892f4d7dca7ed6381c1d2705f349a1
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-05-29 05:52:47 +0300

    add 'nppp_cache_key_regex_probe' to transients array
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/status.php
+++ includes/status.php
@@ -90,6 +90,7 @@ function nppp_clear_plugin_cache($silent = false) {
         'nppp_pages_in_cache_' . md5($static_key_base),
         'nppp_obd_warned_' . md5($static_key_base),
         'nppp_vary_issue_' . md5($static_key_base),
+        'nppp_cache_key_regex_probe',
     );
 
     // Delete each known transient
