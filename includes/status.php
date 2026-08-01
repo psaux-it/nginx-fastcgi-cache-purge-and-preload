d9a501516f5fc63d49bf4c1f73ca5c31fb970d8c
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-08-01 16:40:38 +0300

    use short labes for cron status
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/status.php
+++ includes/status.php
@@ -1232,9 +1232,9 @@ function nppp_my_status_html() {
                             </tr>
                             <tr>
                                 <td class="check"><?php esc_html_e('WP-Cron Reliability', 'fastcgi-cache-purge-and-preload-nginx'); ?></td>
-                                <td class="status" id="npppCronReliability">
+                                <td class="status" id="npppCronReliability" title="<?php echo function_exists('nppp_get_cron_reliability_label') ? esc_attr(nppp_get_cron_reliability_label()) : ''; ?>">
                                     <span class="dashicons"></span>
-                                    <span><?php echo esc_html( function_exists('nppp_get_cron_reliability_label') ? nppp_get_cron_reliability_label() : 'N/A' ); ?></span>
+                                    <span><?php echo esc_html( function_exists('nppp_get_cron_reliability_short_label') ? nppp_get_cron_reliability_short_label() : 'N/A' ); ?></span>
                                 </td>
                             </tr>
                         </tbody>
