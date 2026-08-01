41cd28d3f54a4aa5b9415de67a1fe216f38641bb
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-08-01 16:54:33 +0300

    add cron check transient
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- uninstall.php
+++ uninstall.php
@@ -41,6 +41,7 @@ function nppp_clear_plugin_cache_on_uninstall() {
         'nppp_safexec_version_' . md5($static_key_base),
         'nppp_wget_urls_cache_' . md5($static_key_base),
         'nppp_wget_compatibility_' . md5($static_key_base),
+        'nppp_cron_reliability_' . md5($static_key_base),
         'nppp_missing_commands_' . md5($static_key_base),
         'nppp_preload_phase_' . md5($static_key_base),
         'nppp_preload_cycle_start_' . md5($static_key_base),
