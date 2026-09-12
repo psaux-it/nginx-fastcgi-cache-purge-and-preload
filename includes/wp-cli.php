937942bcd8bfc63b3146cd631a3380ec3f4b4bf6
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-09-13 01:55:30 +0300

    rename 'WP-Cron Reliability' to 'WP-Cron Status'
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/wp-cli.php
+++ includes/wp-cli.php
@@ -462,7 +462,7 @@ class NPPP_CLI_Command extends WP_CLI_Command {
             [ 'Field' => __( 'safexec (Recommended)', 'fastcgi-cache-purge-and-preload-nginx' ),                                              'Value' => $cmd_safexec ],
             [ 'Field' => __( 'rg (Recommended)', 'fastcgi-cache-purge-and-preload-nginx' ),                                                   'Value' => $cmd_rg ],
             [ 'Field' => __( 'cpulimit (Optional)', 'fastcgi-cache-purge-and-preload-nginx' ),                                                'Value' => $cmd_cpulimit ],
-            [ 'Field' => __( 'WP-Cron Reliability', 'fastcgi-cache-purge-and-preload-nginx' ),                                                'Value' => $cron_reliability ],
+            [ 'Field' => __( 'WP-Cron Status', 'fastcgi-cache-purge-and-preload-nginx' ),                                                     'Value' => $cron_reliability ],
 
             $sep( _x( 'CACHE HEALTH', 'status table section header', 'fastcgi-cache-purge-and-preload-nginx' ) ),
             [ 'Field' => __( 'Nginx Cache Path (Required)', 'fastcgi-cache-purge-and-preload-nginx' ),                                        'Value' => $cache_path ],
