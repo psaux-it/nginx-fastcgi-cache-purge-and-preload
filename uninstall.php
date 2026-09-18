f270a95475e618d16d28bdb17de6726a0792cc67
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-09-18 16:02:53 +0300

    add mail health key to uninstall process
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- uninstall.php
+++ uninstall.php
@@ -47,6 +47,7 @@ function nppp_clear_plugin_cache_on_uninstall() {
         'nppp_preload_cycle_start_' . md5($static_key_base),
         'nppp_ping_token_' . md5($static_key_base),
         'nppp_preload_trigger_' . md5($static_key_base),
+        'nppp_mail_health_' . md5($static_key_base),
         'nppp_http_purge_endpoint_broken',
         'nppp_wget_urls_cache_prev_key',
         'nppp_safexec_ok',
