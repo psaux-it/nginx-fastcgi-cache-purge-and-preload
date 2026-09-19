94a40379ce0ab3c8ea2c617d951b3ad99ee1b70e
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2026-09-19 11:10:43 +0300

    add 'nppp_f2b_worker_env' to transients list
    Signed-off-by: Hasan ÇALIŞIR <hasan.calisir@psauxit.com>

--- includes/status.php
+++ includes/status.php
@@ -99,6 +99,7 @@ function nppp_clear_plugin_cache($silent = false) {
         'nppp_setup_strict_detect_' . md5($static_key_base),
         'nppp_requirements_met_' . md5($static_key_base),
         'nppp_f2b_rl',
+        'nppp_f2b_worker_env',
     );
 
     // Delete each known transient
